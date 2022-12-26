2613ed5634afe57a354845959f5743f1c3be8424
iamgabrieltv <gabrielsarcevic1@gmail.com>
2022-12-26 23:26:05 +0100

    Made my website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Hello Hooman!</h1>
+
+<p>
+ I see you stumbled upon my page! I am Gabriel and I do coding and stuff. My current project is this:
+</p>
+
+<div style="width:500px;height:500px;border:0px; background-color:white;">
+ <iframe style="width: 500px; height: 500px;border: 0;" src="https://b.0-0.plus/blog/md.htm?src=https://raw.githubusercontent.com/iamgabrieltv/MC-Streamoverlay-Server/main/README.md"></iframe>
+</div>
+
+<p>I have some more projects! Check them out on my GitHub!</p>
+<a href="https://github.com/iamgabrieltv?tab=repositories">
+ <img alt="GitHub Repos" src="https://img.shields.io/static/v1?label=GitHub Repos&message=2&logo=github&style=for-the-badge&color=191919&labelColor=black&logoColor=white">
+</a>
