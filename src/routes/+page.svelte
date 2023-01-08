00343b2e56b2ba6a73e158b089f089b2c9659e3b
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-01-08 20:56:23 +0100

    Added Stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,38 @@
-<style>
- :global(html, body) {
-  height: 100%;
- }
-
- :global(html) {
-  display: table;
-  margin: auto;
- }
-
- :global(body) {
-  display: table-cell;
-  vertical-align: middle;
-  background-color: #262626;
-  color: white;
-  font-family: Arial, Helvetica, sans-serif;
-  font-weight: bold;
- }
-</style>
+<script>
+	import '../app.css';
+</script>
 
 <h1>Hello Hooman!</h1>
 
 <p>
- I see you stumbled upon my page! I am Gabriel and I do coding and stuff. My current project is this:
+	I see you stumbled upon my page! I am Gabriel and I do coding and stuff. My current project is
+	this:
 </p>
 
 <div style="width:500px;height:500px;border:0px; background-color:white;">
- <iframe style="width: 500px; height: 500px;border: 0;" src="https://b.0-0.plus/blog/md.htm?src=https://raw.githubusercontent.com/iamgabrieltv/MC-Streamoverlay-Server/main/README.md"></iframe>
+	<iframe
+		style="width: 500px; height: 500px;border: 0;"
+		src="https://b.0-0.plus/blog/md.htm?src=https://raw.githubusercontent.com/iamgabrieltv/MC-Streamoverlay-Server/main/README.md"
+	/>
 </div>
 
 <p>I have some more projects! Check them out on my GitHub!</p>
 <a href="https://github.com/iamgabrieltv?tab=repositories">
- <img alt="GitHub Repos" src="https://img.shields.io/static/v1?label=GitHub Repos&message=2&logo=github&style=for-the-badge&color=191919&labelColor=black&logoColor=white">
+	<img
+		alt="GitHub Repos"
+		src="https://img.shields.io/static/v1?label=GitHub Repos&message=2&logo=github&style=for-the-badge&color=191919&labelColor=black&logoColor=white"
+	/>
 </a>
 
-<p>I also contribute to the German Minecraft Server <a href="https://github.com/EarthBuild">Earthbuild</a>.</p>
+<p>
+	I also contribute to the German Minecraft Server <a href="https://github.com/EarthBuild"
+		>Earthbuild</a
+	>.
+</p>
+
+<div class="footer">
+	<p>Made with ❤ in Germany 🇩🇪</p>
+	<a href="https://www.youtube.com/watch?v=KmtzQCSh6xk">
+		<p>!!DO NOT CLICK!!</p>
+	</a>
+</div>
